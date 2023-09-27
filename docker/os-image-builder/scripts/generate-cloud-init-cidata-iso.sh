4ef60f4cd8b538a654e845e0558e9fd87887f281
Michael Chi <kalschi@google.com>
2023-09-27 14:11:37 +0000

    test

--- docker/os-image-builder/scripts/generate-cloud-init-cidata-iso.sh
+++ docker/os-image-builder/scripts/generate-cloud-init-cidata-iso.sh
@@ -114,6 +114,10 @@ while true; do
     echo "ls 3"
     echo "==="
     ls /tmp
+    echo "ls /tmp/workdir"
+    ls /tmp/workdir
+    echo "ls /"
+    ls /
     echo "<=="
     ls "${CLOUD_INIT_DATASOURCE_SOURCE_DIRECTORY_PATH}"
     echo $(ls "${CLOUD_INIT_DATASOURCE_SOURCE_DIRECTORY_PATH}")
