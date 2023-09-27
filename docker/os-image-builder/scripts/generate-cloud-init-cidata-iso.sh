e7d1ff49508f94c2fc26dfd86655e8c09ec8b686
Michael Chi <kalschi@google.com>
2023-09-27 13:44:05 +0000

    up

--- docker/os-image-builder/scripts/generate-cloud-init-cidata-iso.sh
+++ docker/os-image-builder/scripts/generate-cloud-init-cidata-iso.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Copyright 2022 Google LLC
 #
@@ -14,118 +14,79 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -o errexit
 set -o nounset
-set -o pipefail
-
-EXIT_OK=0
-
-ERR_GENERIC=1
-ERR_VARIABLE_NOT_DEFINED=2
-ERR_MISSING_DEPENDENCY=3
-ERR_ARGUMENT_EVAL_ERROR=4
-ERR_ARCHIVE_NOT_SUPPORTED=5
+set -o errexit
 
 echo "This script has been invoked with: $0 $*"
 
-check_argument() {
-  ARGUMENT_VALUE="${1}"
-  ARGUMENT_DESCRIPTION="${2}"
-
-  if [ -z "${ARGUMENT_VALUE}" ]; then
-    echo "[ERROR]: ${ARGUMENT_DESCRIPTION} is not defined. Run this command with the -h option to get help. Terminating..."
-    # Ignoring because those are defined in common.sh, and don't need quotes
-    # shellcheck disable=SC2086
-    exit ${ERR_VARIABLE_NOT_DEFINED}
-  else
-    echo "[OK]: ${ARGUMENT_DESCRIPTION} value is defined: ${ARGUMENT_VALUE}"
-  fi
-
-  unset ARGUMENT_NAME
-  unset ARGUMENT_VALUE
-}
-
-check_exec_dependency() {
-  EXECUTABLE_NAME="${1}"
-
-  if ! command -v "${EXECUTABLE_NAME}" >/dev/null 2>&1; then
-    echo "[ERROR]: ${EXECUTABLE_NAME} command is not available, but it's needed. Make it available in PATH and try again. Terminating..."
-    # Ignoring because those are defined in common.sh, and don't need quotes
-    # shellcheck disable=SC2086
-    exit ${ERR_MISSING_DEPENDENCY}
-  else
-    echo "[OK]: ${EXECUTABLE_NAME} is available in PATH, pointing to: $(command -v "${EXECUTABLE_NAME}")"
-  fi
-
-  unset EXECUTABLE_NAME
-}
-
-echo "Checking if the necessary dependencies are available..."
-check_exec_dependency "cloud-init"
-check_exec_dependency "cloud-localds"
-check_exec_dependency "genisoimage"
-check_exec_dependency "getopt"
+# shellcheck disable=SC1094
+. scripts/common.sh
 
 # Doesn't follow symlinks, but it's likely expected for most users
 SCRIPT_BASENAME="$(basename "${0}")"
 
-CLOUD_INIT_DATASOURCE_OUTPUT_DIRECTORY_PATH_DESCRIPTION="path to the directory that will contain the resulting output"
-CLOUD_INIT_DATASOURCE_SOURCE_DIRECTORY_PATH_DESCRIPTION="path to the directory containing the cloud-init datasource files"
+EDGE_CONFIG_DIRECTORY_PATH_DESCRIPTION="Path to the Edge server configuration files"
+K8S_RUNTIME_DESCRIPTION="kubernetes runtime, can be [anthos], [microk8s] or [k3s]"
 
 usage() {
   echo
-  echo "${SCRIPT_BASENAME} - Build OS images."
+  echo "${SCRIPT_BASENAME} - This script generates the CIDATA ISO."
   echo
   echo "USAGE"
   echo "  ${SCRIPT_BASENAME} [options]"
   echo
   echo "OPTIONS"
-  echo "  -d | --cloud-init-datasource-source-directory: ${CLOUD_INIT_DATASOURCE_SOURCE_DIRECTORY_PATH_DESCRIPTION}"
-  echo "  -o | --cloud-init-datasource-output-directory: ${CLOUD_INIT_DATASOURCE_OUTPUT_DIRECTORY_PATH_DESCRIPTION}"
-  echo "  -h | --help: show this help message and exit"
+  echo "  -d $(is_linux && echo "| --edge-config-directory-path"): ${EDGE_CONFIG_DIRECTORY_PATH_DESCRIPTION}"
+  echo "  -i $(is_linux && echo "| --k8s-runtime"): ${K8S_RUNTIME_DESCRIPTION}"
+  echo "  -h $(is_linux && echo "| --help"): ${HELP_DESCRIPTION}"
   echo
   echo "EXIT STATUS"
   echo
   echo "  ${EXIT_OK} on correct execution."
-  echo "  ${ERR_GENERIC} when an error occurs, and there's no specific error code to handle it."
   echo "  ${ERR_VARIABLE_NOT_DEFINED} when a parameter or a variable is not defined, or empty."
   echo "  ${ERR_MISSING_DEPENDENCY} when a required dependency is missing."
   echo "  ${ERR_ARGUMENT_EVAL_ERROR} when there was an error while evaluating the program options."
-  echo "  ${ERR_ARCHIVE_NOT_SUPPORTED} when the archive is not supported."
 }
 
-if ! TEMP="$(getopt -o d:ho: --long cloud-init-datasource-output-directory:,cloud-init-datasource-source-directory:,help \
-  -n 'build' -- "$@")"; then
-  echo "Terminating..." >&2
-  exit 1
-fi
-eval set -- "$TEMP"
+LONG_OPTIONS="help,edge-config-directory-path:,k8s-runtime:"
+SHORT_OPTIONS="hd:i:"
 
-CLOUD_INIT_DATASOURCE_OUTPUT_DIRECTORY_PATH=
-CLOUD_INIT_DATASOURCE_SOURCE_DIRECTORY_PATH=
+EDGE_CONFIG_DIRECTORY_PATH=
+K8S_RUNTIME=
+
+echo "Checking if the necessary dependencies are available..."
+check_exec_dependency "cp"
+check_exec_dependency "docker"
+check_exec_dependency "getopt"
+check_exec_dependency "rm"
+
+# BSD getopt (bundled in MacOS) doesn't support long options, and has different parameters than GNU getopt
+if is_linux; then
+  TEMP="$(getopt -o "${SHORT_OPTIONS}" --long "${LONG_OPTIONS}" -n "${SCRIPT_BASENAME}" -- "$@")"
+elif is_macos; then
+  TEMP="$(getopt "${SHORT_OPTIONS} --" "$@")"
+  echo "WARNING: Long command line options are not supported on this system."
+fi
+RET_CODE=$?
+if [ ! ${RET_CODE} ]; then
+  echo "Error while evaluating command options. Terminating..."
+  # Ignoring because those are defined in common.sh, and don't need quotes
+  # shellcheck disable=SC2086
+  exit ${ERR_ARGUMENT_EVAL_ERROR}
+fi
+eval set -- "${TEMP}"
 
 while true; do
-  echo "Decoding parameter ${1}..."
   case "${1}" in
-  -d | --cloud-init-datasource-source-directory)
-    CLOUD_INIT_DATASOURCE_SOURCE_DIRECTORY_PATH="${2}"
-    # TODO:
-    echo "${CLOUD_INIT_DATASOURCE_SOURCE_DIRECTORY_PATH}"
-    echo "ls 3"
-    ls "${CLOUD_INIT_DATASOURCE_SOURCE_DIRECTORY_PATH}"
-    echo $(ls "${CLOUD_INIT_DATASOURCE_OUTPUT_DIRECTORY_PATH}")
+  -d | --edge-config-directory-path)
+    EDGE_CONFIG_DIRECTORY_PATH="${2}"
     shift 2
     ;;
-  -o | --cloud-init-datasource-output-directory)
-    CLOUD_INIT_DATASOURCE_OUTPUT_DIRECTORY_PATH="${2}"
-    # TODO:
-    echo "${CLOUD_INIT_DATASOURCE_OUTPUT_DIRECTORY_PATH}"
-    echo "ls 4"
-    echo $(ls "${CLOUD_INIT_DATASOURCE_OUTPUT_DIRECTORY_PATH}")
+  -i | --k8s-runtime)
+    K8S_RUNTIME="${2}"
     shift 2
     ;;
   --)
-    echo "No more parameters to decode"
     shift
     break
     ;;
@@ -138,61 +99,32 @@ while true; do
   esac
 done
 
-echo "Checking if the necessary parameters are set..."
-check_argument "${CLOUD_INIT_DATASOURCE_OUTPUT_DIRECTORY_PATH}" "${CLOUD_INIT_DATASOURCE_OUTPUT_DIRECTORY_PATH_DESCRIPTION}"
-check_argument "${CLOUD_INIT_DATASOURCE_SOURCE_DIRECTORY_PATH}" "${CLOUD_INIT_DATASOURCE_SOURCE_DIRECTORY_PATH_DESCRIPTION}"
+############ Start ABM Prep ######################
+echo "EDGE_CONFIG_DIRECTORY_PATH=${EDGE_CONFIG_DIRECTORY_PATH}"
 
-cloud-init schema --config-file "${CLOUD_INIT_DATASOURCE_SOURCE_DIRECTORY_PATH}/user-data.yaml"
-
-TEMP_CLOUD_INIT_WORKING_DIRECTORY="$(mktemp -d)"
-
-CLOUD_INIT_DATASOURCE_ISO_PATH="${CLOUD_INIT_DATASOURCE_OUTPUT_DIRECTORY_PATH}"/cloud-init-datasource.iso
-
-echo "Removing the eventual leftovers from previous runs..."
-rm -f "${CLOUD_INIT_DATASOURCE_ISO_PATH}"
-
-echo "Copying cloud-init configuration files to ${TEMP_CLOUD_INIT_WORKING_DIRECTORY}..."
-cp \
-  --force \
-  --recursive \
-  --verbose \
-  "${CLOUD_INIT_DATASOURCE_SOURCE_DIRECTORY_PATH}/." "${TEMP_CLOUD_INIT_WORKING_DIRECTORY}/"
-
-echo "Removing the yaml file extension from cloud init datasource configuration files..."
-CLOUD_INIT_METADATA_FILE_PATH="${TEMP_CLOUD_INIT_WORKING_DIRECTORY}/meta-data.yaml"
-if [ -e "${CLOUD_INIT_METADATA_FILE_PATH}" ]; then
-  mv --verbose "${CLOUD_INIT_METADATA_FILE_PATH}" "${TEMP_CLOUD_INIT_WORKING_DIRECTORY}"/meta-data
-else
-  echo "[ERROR] Cannot find the NoCloud datasource meta-data file (${CLOUD_INIT_METADATA_FILE_PATH}). It must be present in a valid NoCloud datasource configuration. Terminating..."
-  # Ignoring because those are defined in common.sh, and don't need quotes
-  # shellcheck disable=SC2086
-  exit ${ERR_ARGUMENT_EVAL_ERROR}
-fi
-CLOUD_INIT_USER_DATA_FILE_PATH="${TEMP_CLOUD_INIT_WORKING_DIRECTORY}/user-data.yaml"
-if [ -e "${CLOUD_INIT_USER_DATA_FILE_PATH}" ]; then
-  mv --verbose "${CLOUD_INIT_USER_DATA_FILE_PATH}" "${TEMP_CLOUD_INIT_WORKING_DIRECTORY}"/user-data
-else
-  echo "[ERROR] Cannot find the NoCloud datasource user-data file (${CLOUD_INIT_USER_DATA_FILE_PATH}). It must be present in a valid NoCloud datasource configuration. Terminating..."
-  # Ignoring because those are defined in common.sh, and don't need quotes
-  # shellcheck disable=SC2086
-  exit ${ERR_ARGUMENT_EVAL_ERROR}
+if [ ! -f "${EDGE_CONFIG_DIRECTORY_PATH}/service-account-key.json" ]; then
+  echo "Service account key file does not exist, please run terraform to create the service account and download JSON key file to ${WORKING_DIRECTORY}/tmp/."
+  exit 1
 fi
 
-# We don't use cloud-localds here because it doesn't support adding data to the
-# ISO, besides user-data, network-config, vendor-data
-echo "Generating the CIDATA ISO..."
-genisoimage \
-  -joliet \
-  -output "${CLOUD_INIT_DATASOURCE_ISO_PATH}" \
-  -rock \
-  -verbose \
-  -volid cidata \
-  "${TEMP_CLOUD_INIT_WORKING_DIRECTORY}"
+echo "cp ${WORKING_DIRECTORY}/media/USB/${K8S_RUNTIME}/meta-data.yaml ${EDGE_CONFIG_DIRECTORY_PATH}/..."
+cp "${WORKING_DIRECTORY}/media/USB/${K8S_RUNTIME}/meta-data.yaml" "${EDGE_CONFIG_DIRECTORY_PATH}/"
+echo "cp ${WORKING_DIRECTORY}/media/USB/${K8S_RUNTIME}/user-data.yaml ${EDGE_CONFIG_DIRECTORY_PATH}/..."
+cp "${WORKING_DIRECTORY}/media/USB/${K8S_RUNTIME}/user-data.yaml" "${EDGE_CONFIG_DIRECTORY_PATH}/"
 
-echo "Deleting the temporary working directory (${TEMP_CLOUD_INIT_WORKING_DIRECTORY})..."
-rm \
-  --force \
-  --recursive \
-  "${TEMP_CLOUD_INIT_WORKING_DIRECTORY}"
+VIAI_INSTALLER_CONFIGURATION_DATA_ISO_DIRECTORY_PATH="$(mktemp -d)"
+echo "Building the OS image builder container image..."
+docker build -t os-image-builder "$(pwd)/docker/os-image-builder"
 
-echo "Results saved in the temporary working directory: ${CLOUD_INIT_DATASOURCE_OUTPUT_DIRECTORY_PATH}"
+echo "Creating the CIDATA ISO in ${VIAI_INSTALLER_CONFIGURATION_DATA_ISO_DIRECTORY_PATH}..."
+
+docker run \
+  -v "${EDGE_CONFIG_DIRECTORY_PATH}":/tmp/cloud-init-source \
+  -v "${VIAI_INSTALLER_CONFIGURATION_DATA_ISO_DIRECTORY_PATH}":/tmp/cloud-init-output \
+  os-image-builder:latest \
+  -d /tmp/cloud-init-source \
+  -oy /tmp/cloud-init-output
+
+echo "CIDATA ISO file created successfully, folder path: ${VIAI_INSTALLER_CONFIGURATION_DATA_ISO_DIRECTORY_PATH} , file name: cloud-init-datasource.iso"
+
+trap 'echo "Cleaning up..."; rm -fr "${EDGE_CONFIG_DIRECTORY_PATH_DESCRIPTION}"; ' EXIT
