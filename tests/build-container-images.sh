552f5b58794f71ced9e7ed1db492ebf0f5c422dc
Marco Ferrari <ferrarim@google.com>
2023-12-11 18:25:50 +0000

    Make container builds parametric

--- tests/build-container-images.sh
+++ tests/build-container-images.sh
@@ -33,7 +33,7 @@ echo "Loaded yq container image ID: ${YQ_CONTAINER_IMAGE_ID}"
 CONTAINER_BUILD_CI_JOB_PATH=".github/workflows/build-container-images.yml"
 
 echo "Loading container image IDs from: ${CONTAINER_BUILD_CI_JOB_PATH}"
-docker run --user "$(id -u)":"$(id -g)" --rm -v "$(pwd)":/workdir "${YQ_CONTAINER_IMAGE_ID}" '.jobs."build-container-images".strategy.matrix."container-image-context-directory"[]' "${CONTAINER_BUILD_CI_JOB_PATH}" | while read -r container_image_id; do
-  echo "Building ${container_image_id} container image"
-  docker build -t "${container_image_id}:latest" "docker/${container_image_id}"
+docker run --user "$(id -u)":"$(id -g)" --rm -v "$(pwd)":/workdir "${YQ_CONTAINER_IMAGE_ID}" '.jobs."build-container-images".strategy.matrix."container-images"[] | ("docker build --tag " + .name + ":latest --file " + .file + " " + .context-directory)' "${CONTAINER_BUILD_CI_JOB_PATH}" | while read -r container_image_build_metadata; do
+  echo "Building the container image with the command: ${container_image_build_metadata}"
+  eval "${container_image_build_metadata}"
 done
