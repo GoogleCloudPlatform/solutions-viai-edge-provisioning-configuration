dc35bf316f8081fd4c634f72dc18c0f582400562
Junho Lee <fakeskimo@gmail.com>
2024-03-14 15:24:24 +0900

    Fixed white space differences with shfmt linter.

--- terraform/gce-vm/startup-script.sh
+++ terraform/gce-vm/startup-script.sh
@@ -24,37 +24,37 @@ apt-get -y upgrade
 
 echo "Installing prerequisites..."
 if ! command -v "kubectl" >/dev/null 2>&1; then
-    echo "Installing kubectl..."
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-    install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+  echo "Installing kubectl..."
+  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 else
-    echo "kubectl already been installed. skip..."
+  echo "kubectl already been installed. skip..."
 fi
 
 if ! command -v "gcloud" >/dev/null 2>&1; then
-    echo "Installing gcloud..."
-    apt-get install -y apt-transport-https ca-certificates gnupg
-    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-    apt-get update -y && apt-get install -y google-cloud-cli
-    echo "gcloud installation completed"
+  echo "Installing gcloud..."
+  apt-get install -y apt-transport-https ca-certificates gnupg
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+  apt-get update -y && apt-get install -y google-cloud-cli
+  echo "gcloud installation completed"
 else
-    echo "gcloud already been installed. skip..."
+  echo "gcloud already been installed. skip..."
 fi
 
 if ! command -v "nvidia-smi" >/dev/null 2>&1; then
-    apt-get install -y openssh-server apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+  apt-get install -y openssh-server apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
-    echo "Installing NVIDIA Drivers..."
-    apt-get install -y nvidia-headless-470-server nvidia-prime nvidia-utils-470-server
+  echo "Installing NVIDIA Drivers..."
+  apt-get install -y nvidia-headless-470-server nvidia-prime nvidia-utils-470-server
 
-    systemctl disable nvidia-suspend.service
-    systemctl disable nvidia-hibernate.service
-    systemctl disable nvidia-resume.service
+  systemctl disable nvidia-suspend.service
+  systemctl disable nvidia-hibernate.service
+  systemctl disable nvidia-resume.service
 
-    rm -f /lib/systemd/system-sleep/nvidia
+  rm -f /lib/systemd/system-sleep/nvidia
 else
-    echo "NVIDIA Drivers already been installed. skip..."
+  echo "NVIDIA Drivers already been installed. skip..."
 fi
 
 # Configures vxlan on the host machine.
@@ -73,9 +73,9 @@ VXLAN_NAME=vxlan104
 # Ubuntu sets almost all devices unmanaged by default, this causes vxlan fails to start
 # So we add vxlan related devices to the configuration file and restart network manager
 if [ ! -f "/etc/NetworkManager/conf.d/10-globally-managed-devices.conf" ]; then
-    touch /etc/NetworkManager/conf.d/10-globally-managed-devices.conf
+  touch /etc/NetworkManager/conf.d/10-globally-managed-devices.conf
 else
-    sed -i 's/unmanaged-devices=*/unmanaged-devices=except:type:ethernet,except:type:vlan,except:type:vxlan,*/g' "/etc/NetworkManager/conf.d/10-globally-managed-devices.conf"
+  sed -i 's/unmanaged-devices=*/unmanaged-devices=except:type:ethernet,except:type:vlan,except:type:vxlan,*/g' "/etc/NetworkManager/conf.d/10-globally-managed-devices.conf"
 fi
 
 systemctl restart NetworkManager
